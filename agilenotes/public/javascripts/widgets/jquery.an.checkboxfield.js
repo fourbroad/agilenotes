b9033bdaf9a0700430df1c34ab0c8d9198a52e85
jk <jk@jk-PC>
2013-05-25 10:15:29 +0000

    修改checkbox

--- agilenotes/public/javascripts/widgets/jquery.an.checkboxfield.js
+++ agilenotes/public/javascripts/widgets/jquery.an.checkboxfield.js
@@ -19,15 +19,45 @@ $.widget( "an.checkboxfield", $.an.inputfield, {
 	},
 
 	_createControl:function(){
-		var self = this, o = this.options;
-		this.input = $("<input type='checkbox'/>").attr({name:o.id})
-		    .addClass("ui-widget-content ui-corner-all").bind("change.checkboxfield",function(e){
+		var self = this, o = this.options, el = this.element;
+		if(o.mobile){
+			el.addClass('codiqa-control ui-field-contain ui-body ui-br')
+			  .bind("mousedown.checkboxfield",function(e){
+				  el.find('label').addClass("ui-btn-down-c");				  
+			  }).bind("mouseup.checkboxfield",function(e){
+				  el.find('label').removeClass("ui-btn-down-c");
+				  if( el.find('input').attr("checked")){
+					  el.find('input').attr("checked",false);
+					  el.find("span .ui-icon").removeClass("ui-icon-checkbox-on").addClass("ui-icon-checkbox-off");
+				  }else{
+					  el.find('input').attr("checked",true);
+					  el.find("span .ui-icon").removeClass("ui-icon-checkbox-off").addClass("ui-icon-checkbox-on");
+				  }
+			  }).bind("mousemove.checkboxfield",function(e){
+				  el.find('label').removeClass("ui-btn-up-c").addClass("ui-btn-hover-c");
+			  }).bind("mouseout.checkboxfield",function(e){
+				  el.find('label').removeClass("ui-btn-hover-c").addClass("ui-btn-up-c");
+			  });	
+			
+			this.input = $("<input type='checkbox'/>").attr({name:o.id})
+		    .addClass("codiqa-control").bind("change.checkboxfield",function(e){
 			var value = self.input.prop("checked"), oldValue = o.value;
 			if(value != oldValue){
 				o.value = value;
 				self._trigger("optionchanged",null,{key:"value", value:value, oldValue:oldValue, isTransient:o.isTransient});
 			}
 		}).bind("dblclick.checkboxfield",function(e){e.stopImmediatePropagation();});
+		}else{
+			this.input = $("<input type='checkbox'/>").attr({name:o.id})
+			    .addClass("ui-widget-content ui-corner-all").bind("change.checkboxfield",function(e){
+				var value = self.input.prop("checked"), oldValue = o.value;
+				if(value != oldValue){
+					o.value = value;
+					self._trigger("optionchanged",null,{key:"value", value:value, oldValue:oldValue, isTransient:o.isTransient});
+				}
+			}).bind("dblclick.checkboxfield",function(e){e.stopImmediatePropagation();});
+		}
+		
 		
 		if(!$.isEmptyObject(o.validate)){
 			this.input.addClass($.toJSON({validate:o.validate}));
@@ -40,7 +70,14 @@ $.widget( "an.checkboxfield", $.an.inputfield, {
 	_createLabel:function(){
 		var o = this.options, el = this.element;
 		if(o.label){
-		    $("<label/>").attr("for",o.id).html(o.label).appendTo(el);
+			if(o.mobile){
+				var labelspan = '<span class="ui-btn-inner"><span class="ui-btn-text">' + o.label+ '</span><span class="ui-icon ui-icon-checkbox-off ui-icon-shadow"> </span></span>'; 
+				$("<label/>").attr({"for":o.id,"class":"ui-checkbox-off ui-btn ui-btn-corner-all ui-fullsize ui-btn-icon-left ui-first-child ui-last-child ui-btn-up-c"})
+							 .html(labelspan).appendTo(el);
+			}else{
+				$("<label/>").attr("for",o.id).html(o.label).appendTo(el);
+			}
+		    
 		}
 	},
 	
