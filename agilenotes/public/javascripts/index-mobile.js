a1deadf44e5837e3d4b511d2c0de6a67f331f62f
lichengzhang2005 <lichengzhang2005@gmail.com>
2013-06-20 19:06:26 +0800

    add toggle field and fix slider toggle height bugs!

--- agilenotes/public/javascripts/index-mobile.js
+++ agilenotes/public/javascripts/index-mobile.js
@@ -93,6 +93,7 @@ document.write('\
 <script src="javascripts/json2.js"><\/script>\
 <script src="javascripts/jsrender.js"><\/script>\
 <script src="javascripts/widgets/jquery.an.sliderwidget.js"><\/script>\
+	<script src="javascripts/widgets/jquery.an.togglefield.js"><\/script>\
 <script src="javascripts/widgets/jquery.an.collapsiblewidget.js"><\/script>\
 <script src="javascripts/widgets/jquery.an.navbarwidget.js"><\/script>\
 <script src="javascripts/widgets/jquery.an.listviewfield.js"><\/script>\
