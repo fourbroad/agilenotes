0334049c0c3510020a640294b802702bf0d0e168
xiaogai <foot3188>
2013-08-01 17:25:15 +0800

    update combin js

--- agilenotes/public/javascripts/agnotes-debug.js
+++ agilenotes/public/javascripts/agnotes-debug.js
@@ -34884,7 +34884,7 @@ $.widget( "an.view", {
 	
 	lastpage:function(){
 		var o = this.options;
-		o.skip = Math.floor(o.total/o.limit)*o.limit-o.limit; 
+		o.skip = Math.floor(o.total/o.limit)*o.limit; 
 		this._loadDocs();
 	},
 
